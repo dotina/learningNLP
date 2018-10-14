e009a7ead43cf1127576467ecad2ea22c969787a
Derrick Dickens Otina <derrick.angolo@live.com>
2018-10-14 13:51:30 +0300

    changes

---
+++ src/main/java/com/ai/nlp/AinlpApplication.java
@@ -0,0 +1,12 @@
+package com.ai.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AinlpApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ai/nlp/names/phoneNums.java
@@ -0,0 +1,33 @@
+package com.ai.nlp.names;
+
+import static java.lang.System.out;
+
+/**
+ * Created by
+ * @author derrick on 14-Oct-18.
+ */
+public class phoneNums {
+    public static void phoneNumbers(){
+        out.println("phone number : 0722345354"+isPhoneNumber("0722345354"));
+        out.println("phone number : 254722345354"+isPhoneNumber("254722345354"));
+        out.println("phone number : 722345354"+isPhoneNumber("722345354"));
+        out.println("phone number : (254)722 345 354"+isPhoneNumber("(254)722 345 354"));
+        out.println();
+        out.println();
+    }
+
+    /**
+     * Phone method
+     * @param s for strign return
+     * @return a true or false
+     */
+    private static boolean isPhoneNumber(String s) {
+        if(s.matches("[0-9]{10}")){
+            return true;
+        }else if(s.matches("\\d{3}\\s\\d{3}\\s\\d{4}")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
